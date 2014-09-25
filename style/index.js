e447b4631350ffda017c758d818a57600f1c3062
HowdenG <graham_howden1@yahoo.co.uk>
2014-09-25 17:14:09 +0100

    feat(generator:style): Add generator for a SASS style
    Adds a SASS partial to the application. Asks for component to be added to.

---
+++ style/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var ScriptBase = require('../scriptbase.js');
+
+
+var AngularjsSilexGenerator = ScriptBase.extend({
+    prompting: function () {
+        var done = this.async();
+
+        var prompts = [
+            {
+                name: 'component',
+                message: 'What component do you want to add the controller to?',
+                default: 'home'
+            }
+        ];
+
+        this.prompt(prompts, function (props) {
+            this.component = props.component;
+            done();
+        }.bind(this));
+    },
+
+    //TODO add the sass partial to the base main.sass
+    writing: function () {
+        this.template('style.sass', this.srcFolder + '/' + this.component + '/_' + this.scriptName + '.sass');
+    }
+});
+
+module.exports = AngularjsSilexGenerator;
