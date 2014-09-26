ed11f8af7efb355e7c77fec3d0a3b4725ea37ba6
HowdenG <graham_howden1@yahoo.co.uk>
2014-09-26 15:06:33 +0100

    feat(generator:style): Automatically add the partial to main.sass on generation

--- style/index.js
+++ style/index.js
@@ -2,15 +2,25 @@
 
 var ScriptBase = require('../scriptbase.js');
 
-
 var AngularjsSilexGenerator = ScriptBase.extend({
     prompting: function() {
         this.componentPrompt();
     },
 
-    //TODO add the sass partial to the base main.sass
     writing: function () {
         this.template('style.sass', this.srcFolder + '/' + this.component + '/_' + this.scriptName + '.sass');
+    },
+
+    install: function() {
+        var file = this.readFileAsString(this.srcFolder + '/main.sass');
+
+        if(file.search(this._escapeRegex('@import ' + this.component + '/' + this.scriptName)) === -1) {
+            this.writeFileFromString(file + '\n@import ' + this.component + '/' + this.scriptName, this.srcFolder + '/main.sass');
+        }
+    },
+
+    _escapeRegex: function(string) {
+        return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
     }
 });
 
