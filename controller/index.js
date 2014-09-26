ee0dd10bb3ae369a7a70bdd2d09c7e46365b0554
HowdenG <graham_howden1@yahoo.co.uk>
2014-09-26 12:43:35 +0100

    feat(generator:controller): Automatically add the script to index.html on creation if it isn't already included

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var ScriptBase = require('../scriptbase.js');
-
+var cheerio = require('cheerio');
+var fs = require('fs');
 
 var AngularjsSilexGenerator = ScriptBase.extend({
     initializing: function () {
@@ -12,9 +13,21 @@ var AngularjsSilexGenerator = ScriptBase.extend({
         this.componentPrompt();
     },
 
-    //TODO add the js file to the base index.html
     writing: function () {
         this.template('controller.js', this.srcFolder + '/' + this.component + '/' + this.scriptName + '.js');
+    },
+
+    install: function() {
+        var file = fs.readFileSync(this.srcFolder + '/index.html');
+        var $ = cheerio.load(file);
+
+        var scriptName = this.srcFolder + '/' + this.component + '/' + this.scriptName + '.js';
+
+        if($('script[src="' + scriptName + '"]').length == 0) {
+            $('script[src="app.js"]').after('\n<script src="' + scriptName +'"></script>');
+        }
+
+        fs.writeFileSync(this.srcFolder + '/index.html', $.html());
     }
 });
 
