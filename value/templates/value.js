e627828f4ed799a560565374446df84eae357723
Graham <graham_howden1@yahoo.co.uk>
2014-09-28 18:24:59 +0100

    feat(generator:value): Initial value generator

---
+++ value/templates/value.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name <%= scriptAppName %>.<%= scriptName %>
+ * @description
+ * # <%= scriptName %>
+ * Value in the <%= appName %>.
+ */
+angular.module('<%= scriptAppName %>')
+    .value('<%= scriptName %>', 'VALUE');
