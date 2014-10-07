6adae4066990eea9423f79ecec1ef343e2a8b56e
HowdenG <graham_howden1@yahoo.co.uk>
2014-10-07 09:21:48 +0100

    fix(grunt): Fix jshint only checking the scripts folder

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -131,7 +131,7 @@ module.exports = function (grunt) {
             all: {
                 src: [
                     'Gruntfile.js',
-                    '<%%= yeoman.app %>/scripts/**/*.js'
+                    '<%%= yeoman.app %>/**/*.js'
                 ]
             },
             test: {
